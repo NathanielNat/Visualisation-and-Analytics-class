ec32266b45d3ee40ebb34b04f4b1e45120c57d25
NathanielNat <psalmnat@gmail.com>
2020-09-13 21:57:57 +0000

    numpy updates

---
+++ dict.py
@@ -0,0 +1,3 @@
+my_dict = { 'school' :'UDS', 'student': {'name':'Ama','id':'2048769203'}}
+
+print(my_dict['student']['id'])
---
+++ my_json.js
@@ -0,0 +1,4 @@
+var myJSON = '{"name":"John", "age":31, "city":"New York"}';
+
+console.log()
+
